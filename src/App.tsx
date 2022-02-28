18675355db6ae8c98cf9192f2456e415ddbbe0e7
Felix Wu <felix@2simple.com>
2022-02-28 22:28:26 +0000

    link firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import Button from "./Button"
 import Display from "./Display"
+import { doc, setDoc } from "firebase/firestore"; 
+import { useEffect } from "react";
+import { getFirestore } from "firebase/firestore"
 
 function App() {
+
+    useEffect(() => {
+        const db = getFirestore()
+        setDoc(doc(db, "cities", "LA"), {
+            name: "Los Angeles",
+            state: "CA",
+            country: "USA",
+            date: (new Date()).getTime()
+          })
+    }, [])
 
     return (
         <div>
---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAvzqnPp6MfmLYeuiESawucUej_Fbchuss",
+  authDomain: "malefizz.firebaseapp.com",
+  projectId: "malefizz",
+  storageBucket: "malefizz.appspot.com",
+  messagingSenderId: "179093081839",
+  appId: "1:179093081839:web:6938bf0579c833eb51551a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+console.log('hi')
