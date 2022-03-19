794cccd9a8e1fdc6107413c61659a30102330d57
Likhitha <m.likhi@yahoo.com>
2022-03-19 19:16:58 +0000

    Generate room code given a code length Release a room code given a code

---
+++ src/generate-room-code.ts
@@ -0,0 +1,34 @@
+import { deleteDoc, doc, getFirestore, setDoc } from "firebase/firestore";
+import { getAllRoomCodes } from "./get-all-room-codes";
+
+export async function generateCode(userId:string, codeLength:number = 4){
+    let code;
+    const usedCodes = new Set(await getAllRoomCodes())
+
+    do {
+      code = getRandomCode(codeLength);
+    } while (usedCodes.has(code));
+    
+    const db = getFirestore()
+    await setDoc(doc(db, "rooms", code), {
+        userIds: [userId],
+    });
+      
+    return code;
+}
+
+export async function releaseCode(roomCode: string){
+    const db = getFirestore()
+    await deleteDoc(doc(db, "rooms", roomCode));
+}
+
+function getRandomCode(codeLength: number): string{
+    //Generates random characters to form a string with a length of codeLength
+    return Array.from(
+        { length: codeLength },
+        () => String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+      ).join('');
+}
+
+
+
---
+++ src/get-all-room-codes.ts
@@ -0,0 +1,7 @@
+import { collection, doc, getDoc, getDocs, getFirestore } from "firebase/firestore";
+
+export async function getAllRoomCodes() {
+    const db = getFirestore()
+    const querySnapshot = await getDocs(collection(db, "rooms"));
+    return querySnapshot.docs.map((doc) => doc.id);
+}
